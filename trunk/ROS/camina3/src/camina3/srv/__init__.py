c0b07f88596d2ea7516d295ccb517ac51a8107cb
mau.rojas25@gmail.com <mau.rojas25@gmail.com@3976d440-f5eb-784e-8f58-fb24377be53c>
2014-02-12 13:15:55 +0000

    -arreglando archivos

--- trunk/ROS/camina3/src/camina3/srv/__init__.py
+++
@@ -1,6 +0,0 @@
-from ._EspacioTrabajoParametros import *
-from ._CinversaParametros import *
-from ._TransTrayectoriaParametros import *
-from ._TransHomogeneaParametros import *
-from ._TrayectoriaEliptica1Parametros import *
-from ._PlanificadorParametros import *
