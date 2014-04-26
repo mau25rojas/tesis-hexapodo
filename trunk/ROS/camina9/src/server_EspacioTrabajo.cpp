b98b7f297c41fba97d3cd0162eae98002fb5cc3a
mau.rojas25@gmail.com <mau.rojas25@gmail.com@3976d440-f5eb-784e-8f58-fb24377be53c>
2014-04-26 01:32:45 +0000

    - hay errores en el codigo... La longitud que recorre la pata es de 18cm (en el actual monetaje), por lo que hay que "trasladar" el espacio de trabajo hacia donde se va a mover (el espacio de trabajo es mucho mas pequeño)

--- trunk/ROS/camina9/src/server_EspacioTrabajo.cpp
+++ trunk/ROS/camina9/src/server_EspacioTrabajo.cpp
@@ -13,8 +13,10 @@ bool simulationRunning=true;
 bool sensorTrigger=false;
 float simulationTime=0.0f;
 float PosicionPata_x=0.0, PosicionPata_y=0.0, PosicionPata_x2=0.0, PosicionPata_y2=0.0, anguloPatas_rad=0.0;
-float rotacionPata[Npatas], phi[Npatas];
+float rotacionPata[Npatas], phi[Npatas], ang_offset=90*pi/180;
 punto3d origenPata[Npatas], Offset;
+//-- Log
+FILE *fp1;
 //-- Funciones
 punto3d TransformacionHomogenea(punto3d Punto_in, punto3d L_traslacion, float ang_rotacion);
 
@@ -45,19 +47,19 @@ bool EspacioTrabajoPatas(camina9::EspacioTrabajoParametros::Request  &req,
     //--Punto1 - izq/arr
     P_aux.x = EspacioTrabajo_X1;
     P_aux.y = EspacioTrabajo_Y1;
-    P1 = TransformacionHomogenea(P_aux,Offset,phi[Npata]+req.alfa);
+    P1 = TransformacionHomogenea(P_aux,Offset,phi[Npata]);//+ang_offset+req.alfa);
     //--Punto2 - der/arr
     P_aux.x = EspacioTrabajo_X2;
     P_aux.y = EspacioTrabajo_Y1;
-    P2 = TransformacionHomogenea(P_aux,Offset,phi[Npata]+req.alfa);
+    P2 = TransformacionHomogenea(P_aux,Offset,phi[Npata]);//+ang_offset+req.alfa);
     //--Punto3 - der/aba
     P_aux.x = EspacioTrabajo_X2;
     P_aux.y = EspacioTrabajo_Y2;
-    P3 = TransformacionHomogenea(P_aux,Offset,phi[Npata]+req.alfa);
+    P3 = TransformacionHomogenea(P_aux,Offset,phi[Npata]);//+ang_offset+req.alfa);
     //--Punto4 - izq/aba
     P_aux.x = EspacioTrabajo_X1;
     P_aux.y = EspacioTrabajo_Y2;
-    P4 = TransformacionHomogenea(P_aux,Offset,phi[Npata]+req.alfa);
+    P4 = TransformacionHomogenea(P_aux,Offset,phi[Npata]);//+ang_offset+req.alfa);
 
 //-- Transformacion 2: a origen de cada pata
     //--Punto1
@@ -83,6 +85,7 @@ bool EspacioTrabajoPatas(camina9::EspacioTrabajoParametros::Request  &req,
     res.EspacioTrabajoP4_x = P4.x;
     res.EspacioTrabajoP4_y = P4.y;
 
+    fprintf(fp1,"%.3f\t%.3f\t%.3f\t%.3f\t%.3f\t%.3f\t%.3f\t%.3f\n",P1.x,P1.y,P2.x,P2.y,P3.x,P3.y,P4.x,P4.y);
 
 //-- Transformacion 3: segun posicion y rotacion del robot
 //    P_Ocuerpo.x = req.PosicionCuerpo_x;
@@ -113,16 +116,19 @@ int main(int argc, char **argv)
 	{
         Offset.x = atof(argv[1]);   //offset de la trayectoria
         Offset.y = atof(argv[2]);   //offset de la trayectoria
-        for(int k=0;k<Npatas;k++) phi[k] = atof(argv[3+k]);
+        for(int k=0;k<Npatas;k++) phi[k]=atof(argv[3+k])*pi/180.0;
     } else {
 		ROS_ERROR("server_EspacioTrabajo: Indique argumentos!\n");
 		return 0;
 	}
     ros::init(argc, argv, "EspacioTrabajo_server");
     ros::NodeHandle n;
+    ROS_INFO("server_EspacioTrabajo just started\n");
 
     ros::Subscriber subInfo=n.subscribe("/vrep/info",1,infoCallback);
     ros::ServiceServer service = n.advertiseService("EspacioTrabajo", EspacioTrabajoPatas);
+
+//    for(int k=0;k<Npatas;k++) ROS_INFO("%.3f\n",phi[k]*180.0/pi);
 
 //    anguloPatas_rad = anguloPatas*pi/180;
     PosicionPata_x = radioCuerpo*cos(anguloPatas);
@@ -153,6 +159,8 @@ int main(int argc, char **argv)
     origenPata[5].x=PosicionPata_x;
     origenPata[5].y=-PosicionPata_y;
     rotacionPata[5]=rotacion_Pata6;
+
+    fp1 = fopen("../fuerte_workspace/sandbox/TesisMaureen/ROS/camina9/datos/LogEDT.txt","w+");
 
   while (ros::ok() && simulationRunning) {
     ros::spinOnce();
